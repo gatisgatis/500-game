0973b2bce8c4fc999340684035af6936e7cc08ea
Gatis <gatis@mobillytx.com>
2021-11-30 22:00:55 +0200

    card from a string method

--- src/main/scala/main/Cards.scala
+++ src/main/scala/main/Cards.scala
@@ -68,6 +68,36 @@ object Cards {
   import Suit._
   import Rank._
 
+  object Card {
+    def fromString(input: String): Option[Card] = {
+      if(input.length != 2) None
+      else {
+        val rankPart = input(0).toUpper
+        val suitPart = input(1).toUpper
+        val rank = rankPart match {
+          case '9' => Some(Nine)
+          case 'T' => Some(Ten)
+          case 'J' => Some(Jack)
+          case 'Q' => Some(Queen)
+          case 'K' => Some(King)
+          case 'A' => Some(Ace)
+          case _ => None
+        }
+        val suit = suitPart match {
+          case 'C' => Some(Club)
+          case 'D' => Some(Diamond)
+          case 'H' => Some(Heart)
+          case 'S' => Some(Spade)
+          case _ => None
+        }
+        for {
+          s <- suit
+          r <- rank
+        } yield Card(s, r)
+      }
+    }
+  }
+
   object Deck {
     private val ranks: Set[Rank] = Set(Nine, Ten, Jack, Queen, King, Ace)
     private val suits: Set[Suit] = Set(Club, Diamond, Heart, Spade)
