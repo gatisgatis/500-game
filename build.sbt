a53277cb6e75811140695cb7dfae35ec4a363262
Gatis <gatis@mobillytx.com>
2022-01-03 23:44:38 +0200

    refactor dependencies

--- build.sbt
+++ build.sbt
@@ -6,22 +6,17 @@ version := "0.1"
 
 scalaVersion := "2.13.7"
 
-val Http4sVersion = "0.22.8"
-val catsVersion = "2.7.0"
-val catsEfVersion = "2.5.4"
-
 scalacOptions := Seq(
   "-Xsource:3",
-//  "-Wconf:msg=\\$implicit\\$:s", // https://github.com/oleg-py/better-monadic-for/issues/50#issuecomment-788150296
 )
 
 libraryDependencies ++= Seq(
-  "org.typelevel" %% "cats-effect" % catsEfVersion,
-  "org.typelevel" %% "cats-core" % catsVersion,
-  "org.http4s" %% "http4s-blaze-server" % Http4sVersion,
-  "org.http4s" %% "http4s-blaze-client" % Http4sVersion,
-  "org.http4s" %% "http4s-dsl" % Http4sVersion,
-  "org.http4s" %% "http4s-circe" % Http4sVersion,
-  "org.http4s" %% "http4s-server" % Http4sVersion,
+  Cats.Core,
+  Cats.Effect,
+  Http4s.BlazeServer,
+  Http4s.BlazeClient,
+  Http4s.Server,
+  Http4s.Circe,
+  Http4s.Dsl,
   Circe.Generic,
 )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,5 @@
 import sbt._
 
-
-
 object Dependencies {
   val CirceVersion = "0.14.1"
   object Circe {
@@ -10,4 +8,19 @@ object Dependencies {
     val Parser = "io.circe" %% "circe-parser" % CirceVersion
     val Optics = "io.circe" %% "circe-optics" % CirceVersion
   }
+  val CatsVersion = "2.7.0"
+  val CatsEffectVersion = "2.5.4"
+  object Cats {
+    val Effect = "org.typelevel" %% "cats-effect" % CatsEffectVersion
+    val Core = "org.typelevel" %% "cats-core" % CatsVersion
+  }
+  val Http4sVersion = "0.22.8"
+  object Http4s {
+    val BlazeServer = "org.http4s" %% "http4s-blaze-server" % Http4sVersion
+    val BlazeClient = "org.http4s" %% "http4s-blaze-client" % Http4sVersion
+    val Dsl = "org.http4s" %% "http4s-dsl" % Http4sVersion
+    val Circe = "org.http4s" %% "http4s-circe" % Http4sVersion
+    val Server = "org.http4s" %% "http4s-server" % Http4sVersion
+  }
+
 }
